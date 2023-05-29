5605bfeb49e805774bbff435fe42d29211b2299e
RFIXATE <s.cancinop@duocuc.cl>
2023-05-29 16:38:25 -0400

    1,6

--- test1.py
+++ test1.py
@@ -26,7 +26,7 @@ start_time_str = start_time.strftime("%b %d %H")
 end_time_str = end_time.strftime("%b %d %H")
 
 # Construir el comando grep para filtrar el archivo de registro
-grep_command = f"grep 'Failed password' /var/log/audit/audit.log | grep '{start_time_str}\|{end_time_str}'"
+grep_command = f"grep 'authentication' /var/log/audit/audit.log | grep '{start_time_str}\|{end_time_str}'"
 
 try:
     # Ejecutar el comando grep y contar las líneas de salida
