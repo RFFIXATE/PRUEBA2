85239ef2c3aeafa4b4169cbc54346b1e918cc057
RFIXATE <s.cancinop@duocuc.cl>
2023-05-29 16:33:30 -0400

    1,6

--- test1.py
+++ test1.py
@@ -11,17 +11,17 @@ import datetime
 now = datetime.datetime.now()
 
 # Calcular la última hora cerrada
-last_hour = now.replace(minute=0, second=0, microsecond=0) - datetime.timedelta(hours=1)
+last_closed_hour = now.replace(minute=0, second=0, microsecond=0) - datetime.timedelta(hours=1)
 
-# Obtener el rango de tiempo para la última hora cerrada
-if last_hour.hour == 0:  # Si la última hora es medianoche
-    start_time = last_hour.replace(hour=23)
-    end_time = last_hour
+# Obtener la fecha y hora de inicio y finalización del rango
+if last_closed_hour.hour == 0:  # Si la última hora es medianoche
+    start_time = last_closed_hour.replace(hour=23)
+    end_time = last_closed_hour
 else:
-    start_time = last_hour.replace(hour=last_hour.hour - 1)
-    end_time = last_hour
+    start_time = last_closed_hour.replace(hour=last_closed_hour.hour - 1)
+    end_time = last_closed_hour
 
-# Formatear el rango de tiempo como cadena
+# Formatear las fechas y horas como cadenas
 start_time_str = start_time.strftime("%b %d %H")
 end_time_str = end_time.strftime("%b %d %H")
 
@@ -33,10 +33,9 @@ try:
     output = subprocess.check_output(grep_command, shell=True)
     failed_attempts = len(output.decode().split('\n')) - 1
 except subprocess.CalledProcessError:
-    # Si no hay coincidencias, mostrar mensaje de 0 intentos fallidos
+    # Si no hay coincidencias, establecer la cantidad de intentos fallidos en 0
     failed_attempts = 0
 
 # Mostrar el resultado
 print(f"Cantidad de intentos fallidos de inicio de sesión en el rango {start_time_str}:00 - {end_time_str}:59: {failed_attempts}")
-
 
