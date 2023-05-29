4ad455466ea51d4628531ee32956987a7b48c030
RFIXATE <s.cancinop@duocuc.cl>
2023-05-28 21:23:04 -0400

    3,4

--- test3.py
+++ test3.py
@@ -5,23 +5,16 @@
 import requests
 import csv
 
-# Realizar la solicitud HTTP al endpoint
 response = requests.get("https://dummyjson.com/quotes")
-
-# Verificar si la solicitud fue exitosa (código de estado 200)
 if response.status_code == 200:
-    # Obtener los datos de la respuesta en formato JSON
     data = response.json()
-
-    # Extraer los valores de los campos "autor" y "texto"
     quotes = data['quotes']
-    values = [[quote['author'], quote['texto']] for quote in quotes]
+    values = [[quote['author'], quote['quote']] for quote in quotes]
 
-    # Guardar los valores en un archivo CSV
     with open("endpoint.csv", mode='w', newline='') as file:
         writer = csv.writer(file, delimiter='\t')
-        writer.writerow(["autor", "texto"])  # Escribir la cabecera
-        writer.writerows(values)  # Escribir los valores
+        writer.writerow(["author", "quote"])
+        writer.writerows(values)
 
     print("Archivo CSV guardado exitosamente.")
 else:
