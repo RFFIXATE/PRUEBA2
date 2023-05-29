3b2d692ee5402462623c4146194be04133730531
RFIXATE <s.cancinop@duocuc.cl>
2023-05-28 21:41:45 -0400

    commit 5

--- test4.py
+++ test4.py
@@ -3,7 +3,7 @@ import csv
 from collections import Counter
 import re
 
-excluded_words = ['the', 'a', 'an', 'and', 'or', 'but', 'for', 'with', 'in', 'on', 'at', 'to', 'from']  # Palabras a excluir en inglés
+excluded_words = ['the', 'a', 'an', 'and', 'or', 'but', 'for', 'with', 'in', 'on', 'at', 'to', 'from', 't']  # Palabras a excluir en inglés
 
 word_counter = Counter()  # Contador de palabras
 
@@ -12,7 +12,7 @@ with open("endpoint.csv", mode='r') as file:
     next(reader)  # Omitir la primera fila (cabecera)
     for row in reader:
         quote = row[1]
-        words = re.findall(r'\b\w+(?:\'\w+)?\b', quote)  # Extraer palabras de la cita
+        words = re.findall(r"\b(?:\w+(?:'\w+)?|\w+)\b", quote)  # Extraer palabras completas con apóstrofes
 
         # Incrementar el contador de palabras (excluyendo las palabras excluidas)
         for word in words:
